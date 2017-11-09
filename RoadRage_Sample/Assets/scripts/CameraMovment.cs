15c661f19ad339b400f4500a3f0966fbac883a02
Tamuz Hod <thod@UCSD.EDU>
2017-11-08 16:29:28 -0800

    Testing

---
+++ RoadRage_Sample/Assets/scripts/CameraMovment.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovment : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        
+        rb = this.GetComponent<Rigidbody>();
+        camera = Camera.main;
+    }
+
+    private Rigidbody rb;
+    public int frameNum = 0;
+    static Vector3 right = new Vector3(1, 0, 0);
+    static Vector3 left = new Vector3(-1, 0, 0);
+    static Vector3 forwed = new Vector3(0, 0, 1);
+    static Vector3 back = new Vector3(0, 0, -1);
+    public float speed = 0.2F;
+    public float force = 2F;
+
+    //public GameObject steeringWheel;
+
+    Camera camera;
+
+    // Update is called once per frame
+    void Update() {
+        Vector3 direction = camera.transform.forward;
+        direction.y = 0;
+        //Debug.Log("x = " + camera.transform.eulerAngles.x + " y =  " + camera.transform.eulerAngles.z  + " z = " + camera.transform.eulerAngles.z);
+        //Debug.Log("direction = " + direction.ToString());
+
+
+
+        rb.AddForce(direction * force);
+
+    }
+
+    public void moveLeft()
+    {
+        transform.position += speed * left;
+
+    }
+
+    public void moveRight()
+    {
+        transform.position += speed * right;
+
+    }
+
+    public void moveForwed()
+    {
+        transform.position += speed * forwed;
+
+    }
+
+    public void moveBack()
+    {
+        transform.position += speed * back;
+    }
+
+}
---
+++ RoadRage_Sample/Assets/scripts/ControllerGrabObject.cs
@@ -0,0 +1,144 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerGrabObject : MonoBehaviour {
+
+    private SteamVR_TrackedObject trackedObj;
+
+    private SteamVR_Controller.Device Controller
+    {
+        get { return SteamVR_Controller.Input((int)trackedObj.index); }
+    }
+   
+    void Awake()
+    {
+        
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+    }
+
+    private GameObject collidingObject;
+    // 2
+    private GameObject objectInHand;
+
+    private void SetCollidingObject(Collider col)
+    {
+
+
+
+        if(col.gameObject == left)
+        {
+            this.transform.parent.gameObject.GetComponent<CameraMovment>().moveLeft();
+            return;
+        }
+
+        if (col.gameObject == right)
+        {
+            this.transform.parent.gameObject.GetComponent<CameraMovment>().moveRight();
+            return;
+        }
+
+        if (col.gameObject == forwed)
+        {
+            this.transform.parent.gameObject.GetComponent<CameraMovment>().moveForwed();
+            return;
+        }
+
+        if (col.gameObject == back)
+        {
+            this.transform.parent.gameObject.GetComponent<CameraMovment>().moveBack();
+            return;
+        }
+
+        // 1
+        if (collidingObject || !col.GetComponent<Rigidbody>())
+        {
+            return;
+        }
+        // 2
+        collidingObject = col.gameObject;
+    }
+
+    // 1
+    public void OnTriggerEnter(Collider other)
+    {
+        SetCollidingObject(other);
+    }
+
+    // 2
+    public void OnTriggerStay(Collider other)
+    {
+        SetCollidingObject(other);
+    }
+
+    // 3
+    public void OnTriggerExit(Collider other)
+    {
+        if (!collidingObject)
+        {
+            return;
+        }
+
+        collidingObject = null;
+    }
+
+    private void GrabObject()
+    {
+        // 1
+        objectInHand = collidingObject;
+        collidingObject = null;
+        // 2
+        var joint = AddFixedJoint();
+        joint.connectedBody = objectInHand.GetComponent<Rigidbody>();
+    }
+
+    // 3
+    private FixedJoint AddFixedJoint()
+    {
+        FixedJoint fx = gameObject.AddComponent<FixedJoint>();
+        fx.breakForce = 20000;
+        fx.breakTorque = 20000;
+        return fx;
+    }
+
+    private void ReleaseObject()
+    {
+        // 1
+        if (GetComponent<FixedJoint>())
+        {
+            // 2
+            GetComponent<FixedJoint>().connectedBody = null;
+            Destroy(GetComponent<FixedJoint>());
+            // 3
+            objectInHand.GetComponent<Rigidbody>().velocity = Controller.velocity;
+            objectInHand.GetComponent<Rigidbody>().angularVelocity = Controller.angularVelocity;
+        }
+        // 4
+        objectInHand = null;
+    }
+
+    public GameObject right;
+    public GameObject left;
+    public GameObject forwed;
+    public GameObject back;
+
+    // Update is called once per frame
+    void Update () {
+        if (Controller.GetHairTriggerDown())
+        {
+            if (collidingObject)
+            {
+                GrabObject();
+            }
+        }
+
+        // 2
+        if (Controller.GetHairTriggerUp())
+        {
+            if (objectInHand)
+            {
+                ReleaseObject();
+            }
+        }
+    }
+}
